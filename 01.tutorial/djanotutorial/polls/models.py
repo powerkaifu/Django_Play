2a3f387781814be111308887e3ddc0141f652d71
KaiFu <powerkaifu@gmail.com>
2024-11-16 17:29:48 +0800

    教學課程_第三部分

--- 01.tutorial/djanotutorial/polls/models.py
+++ 01.tutorial/djanotutorial/polls/models.py
@@ -8,12 +8,11 @@ from django.utils import timezone  # 用於處理時區
 
 # 模組都是 models.Model 的子類別
 class Question(models.Model):
-  question_text = models.CharField(max_length = 200)  # 後台的欄位名稱，易閱讀為主
-  pub_date = models.DateTimeField('date published')
+  question_text = models.CharField('問題描述', max_length = 200)  # 後台的欄位名稱，易閱讀為主
+  pub_date = models.DateTimeField('發布日期')  # 顯示在後台管理介面的名稱
 
-  # 回傳問題的文字內容，用於後台管理介面，會在列表中顯示
   def __str__(self):
-    return self.question_text
+    return self.question_text  # 回傳問題文字，用於後台管理介面，會在列表中顯示，沒有使用會顯示 Question object(1) 這樣的字串
 
   def was_published_recently(self):
     return self.pub_date >= timezone.now() - datetime.timedelta(days = 1)  # 判斷 1 天內發布的問題
--- 01.tutorial/djanotutorial/polls/views.py
+++ 01.tutorial/djanotutorial/polls/views.py
@@ -1,8 +1,41 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import Http404, HttpResponse
+# django.shortcuts 用於簡化代碼，可以減少 try...except... 的使用，降低耦合，建議使用 django.shortcuts
+# 渲染模板用 render 函數
+from django.shortcuts import render, get_object_or_404
+from .models import Question
 
 # Create your views here.
 
 
 def index(request):
-  return HttpResponse("Hello, world. You're at the polls index.")
+  latest_question_list = Question.objects.order_by('-pub_date')[: 5]
+  context = {
+      "latest_question_list": latest_question_list,
+  }  # 上下文
+  return render(request, "polls/index.html", context)  # 渲染模板
+
+
+# def detail(request, question_id):
+#   try:
+#     question = Question.objects.get(pk = question_id)  # pk 是主鍵的縮寫，固定用法
+#   except Question.DoesNotExist:
+#     raise Http404("Question does not exist")
+#   return render(request, "polls/detail.html", { "question": question})
+
+
+# 常用，也可以使用 get_object_or_404() 函數來簡化以上的代碼
+def detail(request, question_id):
+  question = get_object_or_404(Question, pk = question_id)
+  context = {
+      "question": question,
+  }
+  return render(request, "polls/detail.html", context)
+
+
+def results(request, question_id):
+  response = "You're looking at the results of question %s."
+  return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+  return HttpResponse("You're voting on question %s." % question_id)
