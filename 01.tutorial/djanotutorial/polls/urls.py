63feaef966e70e36d09e9bde7b039bbb6d37db6b
KaiFu <powerkaifu@gmail.com>
2024-11-17 15:48:36 +0800

    教學課程_第四部份_表單和通用視圖

--- 01.tutorial/djanotutorial/polls/urls.py
+++ 01.tutorial/djanotutorial/polls/urls.py
@@ -4,13 +4,17 @@ from django.urls import path
 from . import views
 
 app_name = 'polls'  # 為這個應用添加命名空間
+# urlpatterns = [
+#     path('', views.index, name = 'index'),  # ex: /polls/
+#     path('<int:question_id>/', views.detail, name = 'detail'),  # ex: /polls/5/
+#     path('<int:question_id>/results/', views.results, name = 'results'),  # ex: /polls/5/results/
+#     path('<int:question_id>/vote/', views.vote, name = 'vote')  # ex: /polls/5/vote/
+# ]
+
+# 改良後的路由
 urlpatterns = [
-    # ex: /polls/
-    path('', views.index, name = 'index'),
-    # ex: /polls/5/
-    path('<int:question_id>/', views.detail, name = 'detail'),
-    # ex: /polls/5/results/
-    path('<int:question_id>/results/', views.results, name = 'results'),
-    # ex: /polls/5/vote/
+    path('', views.IndexView.as_view(), name = 'index'),
+    path('<int:pk>/', views.DetailView.as_view(), name = 'detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name = 'results'),
     path('<int:question_id>/vote/', views.vote, name = 'vote')
 ]
--- 01.tutorial/djanotutorial/polls/views.py
+++ 01.tutorial/djanotutorial/polls/views.py
@@ -1,8 +1,13 @@
-from django.http import Http404, HttpResponse
+from django.http import Http404, HttpResponse, HttpResponseRedirect
 # django.shortcuts 用於簡化代碼，可以減少 try...except... 的使用，降低耦合，建議使用 django.shortcuts
 # 渲染模板用 render 函數
 from django.shortcuts import render, get_object_or_404
-from .models import Question
+from django.urls import reverse
+from django.db.models import F
+from django.views import generic  # 用於簡化視圖的代碼
+
+# 引入模型
+from .models import Question, Choice
 
 # Create your views here.
 
@@ -32,10 +37,43 @@ def detail(request, question_id):
   return render(request, "polls/detail.html", context)
 
 
-def results(request, question_id):
-  response = "You're looking at the results of question %s."
-  return HttpResponse(response % question_id)
-
-
 def vote(request, question_id):
-  return HttpResponse("You're voting on question %s." % question_id)
+  question = get_object_or_404(Question, pk = question_id)  # 從取得問題開始
+  try:
+    selected_choice = question.choice_set.get(pk = request.POST['choice'])  # 反向選取取得選擇的選項
+  except ( KeyError, Choice.DoesNotExist ):
+    context = {
+        "question": question,
+        "error_message": "You didn't select a choice.",  # 用於前端顯示錯誤訊息
+    }
+    return render(request, "polls/detail.html", context)
+  else:
+    selected_choice.votes = F('votes') + 1  # 可以避免資源競爭，避免使用者同時投票造成不一致
+    selected_choice.save()  # 保存選項
+    return HttpResponseRedirect(reverse('polls:results', args = (question.id,)))  # 重新導向到 results 頁面
+
+
+def results(request, question_id):
+  question = get_object_or_404(Question, pk = question_id)
+  context = { "question": question}
+  return render(request, "polls/results.html", context)
+
+
+# 以下為對應的改良後的路由 ------------------------------------------------------------
+## https://docs.djangoproject.com/zh-hans/5.1/intro/tutorial04/#amend-urlconf
+class IndexView(generic.ListView):
+  template_name = "polls/index.html"
+  context_object_name = "latest_question_list"
+
+  def get_queryset(self):
+    return Question.objects.order_by('-pub_date')[: 5]
+
+
+class DetailView(generic.DetailView):
+  model = Question
+  template_name = "polls/detail.html"
+
+
+class ResultsView(generic.DetailView):
+  model = Question
+  template_name = "polls/results.html"
