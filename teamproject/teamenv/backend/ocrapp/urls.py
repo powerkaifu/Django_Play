3f7376dd7f95544707a3983323447e0d6fc3d1d2
KaiFu <powerkaifu@gmail.com>
2024-11-27 14:20:38 +0800

    save

--- teamproject/teamenv/backend/ocrapp/urls.py
+++ teamproject/teamenv/backend/ocrapp/urls.py
@@ -2,12 +2,11 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import ImageUploadViewSet
 
-
-router = DefaultRouter() # 配置 DefaultRouter
+router = DefaultRouter()  # 配置 DefaultRouter
 
 ## r'upload'：路由的前綴，例如 http://127.0.0.1:8000/api/upload/
-## basename：路由的名稱，
-router.register(r'upload', ImageUploadViewSet, basename = 'upload') # 註冊路由
+## basename：路由的名稱
+router.register(r'upload', ImageUploadViewSet, basename = 'upload')  # 註冊路由
 
 urlpatterns = [
     path('', include(router.urls)),
