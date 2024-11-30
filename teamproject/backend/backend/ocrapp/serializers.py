1bef6e17dd277f6d4fbe0eee6d04ebf6f3d0bdf0
KaiFu <powerkaifu@gmail.com>
2024-11-30 16:19:42 +0800

    save

--- teamproject/backend/backend/ocrapp/serializers.py
+++ teamproject/backend/backend/ocrapp/serializers.py
@@ -1,15 +1,17 @@
-# 序列化器（Serializer）在 Django REST framework (DRF) 中扮演著數據轉換和驗證的角色。
-# 它們負責將複雜的數據類型（如 Django 模型實例或查詢集）轉換為 Python 原生數據類型，
-# 這些數據類型可以輕鬆地轉換為 JSON、XML 或其他內容類型，並且可以將輸入的數據轉換回複雜的數據類型。
-# 例如 querysets 和 model instances，轉換為 Python 原生數據類型，以便於 JSON、XML 或其他內容類型的渲染。
+'''
+序列化器是 Django REST Framework (DRF) 中用於處理數據的橋樑，主要負責：
+驗證前端傳入的數據（JSON 格式）。
+將數據轉換為後端的 Python 對象，或將後端對象轉為 JSON 格式返回給前端。
+'''
+
 from rest_framework import serializers
 from .models import UploadImage
 
 
 # 定義了一個序列化器（Serializer）
-class uploadSerializer(serializers.ModelSerializer):
+class UploadImageSerializer(serializers.ModelSerializer):
 
   class Meta:
     model = UploadImage  # 指定要被序列化的模型，這是在 models.py 中定義的模型
     fields = '__all__'  # 指定要被序列化的所有欄位
-    # fields = ['image']  # 指定要被序列化的模型欄位，這個也是在 models.py 中定義的
+    #  fields = ['image', 'translated_content', 'title'] # 指定要被序列化的模型欄位，這個也是在 models.py 中定義的
--- teamproject/backend/backend/ocrapp/views.py
+++ teamproject/backend/backend/ocrapp/views.py
@@ -6,7 +6,7 @@ from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework.parsers import MultiPartParser, FormParser, JSONParser
 from rest_framework.decorators import action
-from .serializers import uploadSerializer
+from .serializers import UploadImageSerializer
 from PIL import Image
 import pytesseract
 # import openai
@@ -21,37 +21,48 @@ class ImageUploadViewSet(viewsets.ViewSet):
 
   @action(detail=False, methods=['post'])
   def translate(self, request):
-    # print(request.data)
-    serializer = uploadSerializer(data=request.data)  # 將上傳的圖片資料，交給序列化器處理
-    if serializer.is_valid():
-        image = Image.open(request.data['image'])  # 使用 PIL 打開圖片
-        # 進行辨識圖片中的文字
-        ocr_text = pytesseract.image_to_string(
-            image,
-            lang='kor+jpn+eng+chi_tra+chi_sim',
-            # lang='kor+jpn+eng+chi_tra+chi_sim+fra+deu+spa+ita+rus+por+ara+hin+tha+vie',
-        )
-        ocr_text = ocr_text.replace('\n', ' ')   # 拿掉換行符號
-        ocr_text = ocr_text.replace('. ', '.\n') # 遇到 . 換行
-        return Response({'data': ocr_text}, status=status.HTTP_200_OK)
-    else:
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    try:
+          image = Image.open(request.data['image'])  # 使用 PIL 打開圖片
+          # 進行辨識圖片中的文字
+          ocr_text = pytesseract.image_to_string(
+              image,
+              lang='kor+jpn+eng+chi_tra+chi_sim+fra+deu+spa+ita+rus+por+ara+hin+tha+vie',
+          )
+          ocr_text = ocr_text.replace('\n', ' ')   # 拿掉換行符號
+          ocr_text = ocr_text.replace('. ', '.\n') # 遇到 . 換行
+          return Response({'data': ocr_text}, status=status.HTTP_200_OK)
+    except Exception as e:
+        return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
   # 新增，對應前端的 POST 請求
   def create(self, request, *args, **kwargs):
-        print(request.data)
-        serializer = uploadSerializer(data=request.data)  # 將上傳的資料，交給序列化器處理
-        if serializer.is_valid():
-            upload_image = serializer.save()  # 儲存上傳圖片的圖片路徑，且會回傳一個 UploadImage 實例
-            translated_content = request.data.get('translated_content')   # 獲取前端傳來的翻譯後的文字
-            title = request.data.get('title')   # 獲取前端傳來的標題
-            upload_image.title = title
-            upload_image.translated_content = translated_content
-            upload_image.save() # 儲存翻譯結果到資料庫
+      try:
+          # 將前端資料，交給序列化器處理
+          serializer = UploadImageSerializer(data=request.data)
 
-            return Response({'message':'新增成功','data': translated_content, 'id': upload_image.id}, status=status.HTTP_201_CREATED)
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+          # 必須先驗證資料是否合法
+          serializer.is_valid(raise_exception=True)
+          upload_image = serializer.save()
+
+          # 獲取前端傳來的資料
+          translated_content = request.data.get('translated_content')
+          title = request.data.get('title')
+
+          # 構建更新欄位的字典
+          update_fields = {
+              'translated_content': translated_content,
+              'title': title
+          }
+
+          # 一次更新多個欄位
+          for field, value in update_fields.items():
+              setattr(upload_image, field, value)
+
+          # 儲存翻譯結果到資料庫
+          upload_image.save()
+          return Response({'message': '新增成功'}, status=status.HTTP_201_CREATED)
+      except Exception as e:
+          return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
   # 更新，對應前端的 PUT 請求(這邊前端用 modal 來做)
   def update(self, request, *args, **kwargs):
@@ -66,17 +77,19 @@ class ImageUploadViewSet(viewsets.ViewSet):
     except Exception as e:
       return Response({ 'message': str(e)}, status = status.HTTP_400_BAD_REQUEST)
 
-  def list(self, request):
-    upload_images = UploadImage.objects.all()
-    serializer = uploadSerializer(upload_images, many = True) # 將每一個 UploadImage 實例序列化為 JSON 格式
-    return Response(serializer.data, status = status.HTTP_200_OK)
-
+  # 刪除，對應前端的 DELETE 請求
   def destroy(self, request, *args, **kwargs):
     try:
       upload_image = UploadImage.objects.get(id=kwargs.get('pk'))
       upload_image.delete()
-      return Response({'message':'刪除成功'}, status=status.HTTP_204_NO_CONTENT)
+      return Response({'message':'刪除成功'}, status = status.HTTP_200_OK)
     except UploadImage.DoesNotExist:
-      return Response({'message':'未找到對應的紀錄'},status=status.HTTP_400_NOT_FOUND)
+      return Response({'message':'未找到對應的紀錄'},status=status.HTTP_404_NOT_FOUND)
     except Exception as e:
-      return Response({'messsage':str(e)},status=status.HTTP_400_BAD_REQUEST)
+      return Response({'message':str(e)},status=status.HTTP_400_BAD_REQUEST)
+
+  # 查詢，對應前端的 GET 請求
+  def list(self, request):
+    upload_images = UploadImage.objects.all()
+    serializer = UploadImageSerializer(upload_images, many = True)
+    return Response(serializer.data, status = status.HTTP_200_OK)
